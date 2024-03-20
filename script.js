6dd8be46c3fdc5670cc4c3dd4764e48e3f112ba2
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-03-20 05:35:18 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ const frequencyMap = {};
+let minKey=null;
+    for (let char of str) {
+        frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+    }
+    
+    let minValue=Infinity;
+    for(let key in frequencyMap){
+           console.log(key)
+           if(frequencyMap[key]<minValue){
+               minValue = frequencyMap[key];
+               minKey=key;
+           }
+    }
+    
+    if(minValue==1){
+          console.log(minKey)
+    }
+    else{
+          console.log("null")
+    }
+    
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
